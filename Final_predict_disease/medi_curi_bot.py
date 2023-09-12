514c5dc10e1ebc87bcbcdb7d0be34392db932d4d
FenilPatel228 <137047717+FenilPatel228@users.noreply.github.com>
2023-09-12 22:29:15 +0530

    Add files via upload

---
+++ Final_predict_disease/medi_curi_bot.py
@@ -0,0 +1,312 @@
+import logging
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    filters,
+    ContextTypes,
+)
+import pickle
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+from pbl_prd_py import predict_disease
+data = pd.read_csv('Training.csv')
+X = data.drop('prognosis', axis=1)
+y = data['prognosis']
+des = pd.read_csv('symptom_Description.csv')
+pre = pd.read_csv('symptom_precaution.csv')
+sym_list='''
+◉ Abdominal pain
+◉ Abnormal menstruation
+◉ Acidity
+◉ Acute liver failure
+◉ Altered sensorium
+◉ Anxiety
+◉ Back pain
+◉ Belly pain
+◉ Blackheads
+◉ Bladder discomfort
+◉ Blister
+◉ Blood in sputum
+◉ Bloody stool
+◉ Blurred and distorted vision
+◉ Breathlessness
+◉ Brittle nails
+◉ Bruising
+◉ Burning micturition
+◉ Chest pain
+◉ Chills
+◉ Cold hands and feets
+◉ Congestion
+◉ Constipation
+◉ Continuous feel of urine
+◉ Continuous sneezing
+◉ Cough
+◉ Cramps
+◉ Dark urine
+◉ Dehydration
+◉ Depression
+◉ Diarrhoea
+◉ Dischromic  patches
+◉ Distention of abdomen
+◉ Dizziness
+◉ Drying and tingling lips
+◉ Enlarged thyroid
+◉ Excessive hunger
+◉ Extra marital contacts
+◉ Family history
+◉ Fast heart rate
+◉ Fatigue
+◉ Fluid overload
+◉ Foul smell of urine
+◉ Headache
+◉ High fever
+◉ Hip joint pain
+◉ History of alcohol consumption
+◉ Increased appetite
+◉ Indigestion
+◉ Inflammatory nails
+◉ Internal itching
+◉ Irregular sugar level
+◉ Irritability
+◉ Irritation in anus
+◉ Itching
+◉ Joint pain
+◉ Knee pain
+◉ Lack of concentration
+◉ Lethargy
+◉ Loss of appetite
+◉ Loss of balance
+◉ Loss of smell
+◉ Malaise
+◉ Mild fever
+◉ Mood swings
+◉ Movement stiffness
+◉ Mucoid sputum
+◉ Muscle pain
+◉ Muscle wasting
+◉ Muscle weakness
+◉ Nausea
+◉ Neck pain
+◉ Nodal skin eruptions
+◉ Obesity
+◉ Pain behind the eyes
+◉ Pain during bowel movements
+◉ Pain in anal region
+◉ Painful walking
+◉ Palpitations
+◉ Passage of gases
+◉ Patches in throat
+◉ Phlegm
+◉ Polyuria
+◉ Prominent veins on calf
+◉ Puffy face and eyes
+◉ Pus filled pimples
+◉ Receiving blood transfusion
+◉ Receiving unsterile injections
+◉ Red sore around nose
+◉ Red spots over body
+◉ Redness of eyes
+◉ Restlessness
+◉ Runny nose
+◉ Rusty sputum
+◉ Scurring
+◉ Shivering
+◉ Silver like dusting
+◉ Sinus pressure
+◉ Skin peeling
+◉ Skin rash
+◉ Slurred speech
+◉ Small dents in nails
+◉ Spinning movements
+◉ Spotting  urination
+◉ Stiff neck
+◉ Stomach bleeding
+◉ Stomach pain
+◉ Sunken eyes
+◉ Sweating
+◉ Swelled lymph nodes
+◉ Swelling joints
+◉ Swelling of stomach
+◉ Swollen blood vessels
+◉ Swollen extremeties
+◉ Swollen legs
+◉ Throat irritation
+◉ Toxic look (typhos)
+◉ Ulcers on tongue
+◉ Unsteadiness
+◉ Visual disturbances
+◉ Vomiting
+◉ Watering from eyes
+◉ Weakness in limbs
+◉ Weakness of one body side
+◉ Weight gain
+◉ Weight loss
+◉ Yellow crust ooze
+◉ Yellow urine
+◉ Yellowing of eyes
+◉ Yellowish skin
+'''
+with open('bayes_dis_model.pkl', 'rb') as bayes:
+    bayes_model = pickle.load(bayes)
+    
+with open('rf_dis_model.pkl', 'rb') as rf:
+    random_forest_model = pickle.load(rf)
+
+with open('xg_dis_model.pkl', 'rb') as xg:
+    xgboost_model = pickle.load(xg)
+
+# Initialize logging and other settings
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Conversation states
+SYMPTOM1, SYMPTOM2, SYMPTOM3, SYMPTOM4, SYMPTOMs, PREDICTION = range(6)
+
+# Initialize the list to store symptoms
+user_symptoms = []
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Welcome to the Health Chatbot! Please describe the symptoms you are experiencing."
+    )
+    return SYMPTOM1
+
+async def collect_symptoms_1(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    symptom = update.message.text.lower().strip().replace(" ","_")
+    
+    # Check if the symptom is valid
+    if symptom in X.columns:
+        if symptom not in user_symptoms:
+            user_symptoms.append(symptom)
+            # break
+        else:
+            await update.message.reply_text(f"You've already mentioned {symptom}. Please describe another symptom.")
+            return SYMPTOM1
+    else:
+        await update.message.reply_text(f"Please enter a valid symptom. For reference, take a look at below list...\n{sym_list}")
+        return SYMPTOM1
+
+    await update.message.reply_text(f"Please enter a symptom you're experiencing other than {symptom}.")
+    return SYMPTOM2
+
+async def collect_symptoms_2(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    symptom = update.message.text.lower().strip().replace(" ","_")
+        
+        # Check if the symptom is valid
+    if symptom in X.columns:
+        if symptom not in user_symptoms:
+            user_symptoms.append(symptom)
+        else:
+            await update.message.reply_text(f"You've already mentioned {symptom}. Please describe another symptom.")
+            return SYMPTOM2
+    else:
+        await update.message.reply_text(f"Please enter a valid symptom. For reference, take a look at below list...\n{sym_list}")
+        return SYMPTOM2
+    await update.message.reply_text(f"Please enter a symptom you're experiencing other than {symptom}.")
+    return SYMPTOM3
+
+async def collect_symptoms_3(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    symptom = update.message.text.lower().strip().replace(" ","_")
+    
+    # Check if the symptom is valid
+    if symptom in X.columns:
+        if symptom not in user_symptoms:
+            user_symptoms.append(symptom)
+        else:
+            await update.message.reply_text(f"You've already mentioned {symptom}. Please describe another symptom.")
+            return SYMPTOM3
+    else:
+        await update.message.reply_text(f"Please enter a valid symptom. For reference, take a look at below list...\n{sym_list}")
+        return SYMPTOM3
+    await update.message.reply_text(f"Please enter a symptom you're experiencing other than {symptom}.")
+    return SYMPTOM4
+
+async def collect_symptoms_4(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    symptom = update.message.text.lower().strip().replace(" ","_")
+    
+    # Check if the symptom is valid
+    if symptom in X.columns:
+        if symptom not in user_symptoms:
+            user_symptoms.append(symptom)
+        else:
+            await update.message.reply_text(f"You've already mentioned {symptom}. Please describe another symptom.")
+            return SYMPTOM4
+    else:
+        await update.message.reply_text(f"Please enter a valid symptom. For reference, take a look at below list...\n{sym_list}")
+        return SYMPTOM4
+            
+    await update.message.reply_text("Are you experiencing any other symptoms? (Yes/No)")
+    return SYMPTOMs
+    
+
+async def collect_symptoms(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    var = update.message.text
+    
+    if var == "yes" or var == 'Yes':
+        await update.message.reply_text("What is the symptom that you're experiencing: ")
+        return SYMPTOM4
+    elif (var !='yes' and var != 'Yes' and var!='no' and var != 'No'):
+        await update.message.reply_text("Invalid input. Please respond with 'Yes' or 'No'.")
+        return SYMPTOMs
+    await update.message.reply_text("Enter 'Predict' to get prediction.")
+    return PREDICTION
+        
+
+async def pre_disease(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Predict the disease based on the collected symptoms
+    predicted_disease = predict_disease(user_symptoms)
+
+    # Build a response message
+    response_message = "Based on the symptoms you provided, you may have: \n"
+    response_message += "\n".join(predicted_disease)
+
+    # Provide descriptions and precautions for predicted diseases (you can modify this part)
+    for disease in predicted_disease:
+        description = des[des['Name'] == disease].values[0][1]
+        precautions = pre[pre['Name'] == disease].values[0][1:]
+
+        response_message += f"\n\n{disease}:\n{description}\nPrecautions and measures you should take:\n"
+        for i, precaution in enumerate(precautions, start=1):
+            response_message += f"{i}. {precaution}\n"
+
+    # Send the response message to the user
+    await update.message.reply_text(response_message)
+
+    # Clear the user's symptoms for the next conversation
+    user_symptoms.clear()
+
+    # End the conversation
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Conversation canceled. Type /start to begin a new conversation.')
+    return ConversationHandler.END
+
+def main() -> None:
+    application = Application.builder().token("6358213237:AAF3YW4w6ilkcHCvWKibF5TlP7q68Yon4sc").build()
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            SYMPTOM1: [MessageHandler(filters.TEXT & ~filters.COMMAND, collect_symptoms_1)],
+            SYMPTOM2: [MessageHandler(filters.TEXT & ~filters.COMMAND, collect_symptoms_2)],
+            SYMPTOM3: [MessageHandler(filters.TEXT & ~filters.COMMAND, collect_symptoms_3)],
+            SYMPTOM4: [MessageHandler(filters.TEXT & ~filters.COMMAND, collect_symptoms_4)],
+            SYMPTOMs: [MessageHandler(filters.TEXT & ~filters.COMMAND, collect_symptoms)],
+            PREDICTION: [MessageHandler(filters.TEXT & ~filters.COMMAND, pre_disease)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conversation_handler)
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    main()
+    
+
---
+++ Final_predict_disease/pbl_prd_py.py
@@ -0,0 +1,97 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+data = pd.read_csv('training.csv')
+data.head()
+X = data.drop('prognosis', axis=1)
+y = data['prognosis']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+le = preprocessing.LabelEncoder()
+le.fit(y_train)
+y_train_encoded = le.transform(y_train)
+y_test_encoded = le.transform(y_test)
+random_forest_model = RandomForestClassifier(n_estimators=100, random_state=42)
+xgboost_model = XGBClassifier(n_estimators=100, objective='multi:softprob', random_state=42)
+bayes = GaussianNB()
+# tree = DecisionTreeClassifier(random_state=42)
+# svc = SVC(random_state=42)
+random_forest_model.fit(X_train, y_train_encoded)
+xgboost_model.fit(X_train, y_train_encoded)
+bayes.fit(X_train, y_train_encoded)
+# tree.fit(X_train, y_train_encoded)
+# svc.fit(X_train, y_train_encoded)
+rf_prd = random_forest_model.predict(X_test)
+xgboost_prd = xgboost_model.predict(X_test)
+bayes_prd = bayes.predict(X_test)
+# tree_prd = tree.predict(X_test)
+# svc_prd = svc.predict(X_test)
+rf_acc = accuracy_score(y_test_encoded, rf_prd)
+xgboost_acc = accuracy_score(y_test_encoded, xgboost_prd)
+bayes_acc = accuracy_score(y_test_encoded, bayes_prd)
+# tree_acc = accuracy_score(y_test_encoded, tree_prd)
+# svc_acc = accuracy_score(y_test_encoded, svc_prd)
+print(rf_acc,xgboost_acc,bayes_acc)
+def predict_disease(symptoms):
+    # Convert the input symptom names into a binary format using the column names
+    input_data = pd.DataFrame([[1 if col in symptoms else 0 for col in X.columns]], columns=X.columns)
+    
+    # Make predictions using individual models
+    rf_prediction = random_forest_model.predict(input_data)
+    xgboost_prediction = xgboost_model.predict(input_data)
+    bayes_prediction = bayes.predict(input_data)
+    # tree_prediction = tree.predict(input_data)
+    # svc_prediction = svc.predict(input_data)
+    
+    # Decode the predicted label using the label encoder
+    # predicted_disease = np.array([le.inverse_transform([rf_prediction]),le.inverse_transform([xgboost_prediction]),le.inverse_transform([bayes_prediction]),le.inverse_transform([tree_prediction]),le.inverse_transform([svc_prediction])])
+    predicted_disease = np.array([le.inverse_transform([rf_prediction]),le.inverse_transform([xgboost_prediction]),le.inverse_transform([bayes_prediction])])
+    predicted_disease = np.unique(predicted_disease)
+    
+    # res = f"""
+    # rf_prediction = {le.inverse_transform([rf_prediction])}
+    # xgboost_prediction = {le.inverse_transform([xgboost_prediction])}
+    # bayes_prediction = {le.inverse_transform([bayes_prediction])}
+    # """
+    
+    # print(res)
+    
+    return (predicted_disease)
+
+# Example usage
+input_symptoms = ["high_fever", "nausea", "pain_behind_the_eyes", 'headache']
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+input_symptoms = ["fatigue", "restslessness", "diarrhoea", 'vomiting']
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+input_symptoms = ["fatigue", "itching", "headache", 'vomiting', 'high_fever']
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+input_symptoms = ["nausea", "itching", "headache", 'vomiting', 'high_fever']
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+input_symptoms = ["fatigue", "cough", "headache", 'chills', 'high_fever', 'dizziness']
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+input_symptoms = ["high_fever"]
+predicted_disease = predict_disease(input_symptoms)
+print("Predicted Disease:", predicted_disease)
+# import pickle
+
+# with open('rf_dis_model.pkl', 'wb') as model_rf:
+#     pickle.dump(random_forest_model, model_rf)
+    
+# with open('xg_dis_model.pkl', 'wb') as model_xg:
+#     pickle.dump(xgboost_model, model_xg)
+
+# with open('bayes_dis_model.pkl', 'wb') as model_bayes:
+#     pickle.dump(bayes, model_bayes)
